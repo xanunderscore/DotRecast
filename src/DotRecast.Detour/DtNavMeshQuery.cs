de3af0fc5db6f0cd726a5dfe6030b09128be60e5
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-03-09 12:04:16 +0000

    Experiment: always use edge intersection point for pathfinding.

--- src/DotRecast.Detour/DtNavMeshQuery.cs
+++ src/DotRecast.Detour/DtNavMeshQuery.cs
@@ -882,13 +882,13 @@ namespace DotRecast.Detour
 
                     // If the node is visited the first time, calculate node position.
                     var neighbourPos = neighbourNode.pos;
-                    var empStatus = neighbourRef == endRef
-                        ? GetEdgeIntersectionPoint(bestNode.pos, bestRef, bestPoly, bestTile,
+                    var empStatus = /*neighbourRef == endRef
+                        ?*/ GetEdgeIntersectionPoint(bestNode.pos, bestRef, bestPoly, bestTile,
                             endPos, neighbourRef, neighbourPoly, neighbourTile,
                             ref neighbourPos)
-                        : GetEdgeMidPoint(bestRef, bestPoly, bestTile,
+                        /*: GetEdgeMidPoint(bestRef, bestPoly, bestTile,
                             neighbourRef, neighbourPoly, neighbourTile,
-                            ref neighbourPos);
+                            ref neighbourPos)*/;
 
                     // Calculate cost and heuristic.
                     float cost = 0;
